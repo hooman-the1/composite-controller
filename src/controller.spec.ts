30046e41f4da1ada9baeb889701b0773767d458b
Amirali Momtaheni <amiralimomtaheni@gmail.com>
2023-03-23 21:20:20 +0330

    adding children with new branch scenario tests passed

--- src/controller.spec.ts
+++ src/controller.spec.ts
@@ -1,4 +1,3 @@
-
 import "reflect-metadata";
 import { ControllerClass } from "./controller.class.js";
 import { Conductor } from "./controller.interface.js";
@@ -10,62 +9,82 @@ jest.mock("./controller.class");
 const controllerDecorator = Controller;
 
 describe("Controller decorator", () => {
-    const endPoint = "/v1/courses/single";
-    let conductors: Conductor[];
-    let v1: Conductor;
-    let v1Courses: Conductor;
-    let v1CoursesSingle: Conductor;
-    let v2: Conductor;
-    let v2Courses: Conductor;
-    let v2Tests: Conductor;
-    let v3: Conductor;
-    let v3Test1: Conductor;
-    let v3Courses: Conductor;
-    let v3Test3: Conductor;
+  const endPoint = "/v1/courses/single";
+  let conductors: Conductor[];
+  let v1: Conductor;
+  let v1Courses: Conductor;
+  let v1CoursesSingle: Conductor;
+  let v2: Conductor;
+  let v2Courses: Conductor;
+  let v2Tests: Conductor;
+  let v3: Conductor;
+  let v3Test1: Conductor;
+  let v3Courses: Conductor;
+  let v3Test3: Conductor;
 
-    beforeEach(() => {
-        controllerDecorator("/v1");
-        controllerDecorator("/v1/courses");
-        controllerDecorator("/v2/courses");
-        controllerDecorator("/v2/tests");
-        controllerDecorator("/v3/test1/courses/test3");
-        controllerDecorator("/v3/test1");
-        controllerDecorator(endPoint);
-        conductors = container.getAll<Conductor>(ControllerType.Conductor).sort((a, b) => a.depth - b.depth);
-        v1 = conductors.find(element => element.name === "v1")!;
-        v1Courses = v1?.children[0];
-        v1CoursesSingle = v1Courses?.children[0];
-        v2 = conductors.find(element => element.name === "v2")!;
-        v2Courses = v2?.children.find(element => element.name === "courses")!;
-        v2Tests = v2?.children.find(element => element.name === "tests")!;
-        v3 = conductors.find(element => element.name === "v3")!;
-        v3Test1 = v3?.children[0];
-        v3Courses = v3Test1?.children[0];
-        v3Test3 = v3Courses?.children[0];
-    })
+  beforeEach(() => {
+    controllerDecorator("/v1");
+    controllerDecorator("/v1/courses");
+    controllerDecorator("/v2/courses");
+    controllerDecorator("/v2/tests");
+    controllerDecorator("/v3/test1/courses/test3");
+    controllerDecorator("/v3/test1");
+    controllerDecorator(endPoint);
+    conductors = container
+      .getAll<Conductor>(ControllerType.Conductor)
+      .sort((a, b) => a.depth - b.depth);
+    // v1 = conductors.find((element) => element.name === "v1")!;
+    // v1Courses = v1?.children[0];
+    // v1CoursesSingle = v1Courses?.children[0];
+    // v2 = conductors.find((element) => element.name === "v2")!;
+    // v2Courses = v2?.children.find((element) => element.name === "courses")!;
+    // v2Tests = v2?.children.find((element) => element.name === "tests")!;
+    // v3 = conductors.find((element) => element.name === "v3")!;
+    // v3Test1 = v3?.children[0];
+    // v3Courses = v3Test1?.children[0];
+    // v3Test3 = v3Courses?.children[0];
+  });
 
-    afterEach(() => {
-        container.unbindAll();
-    })
+  afterEach(() => {
+    container.unbindAll();
+  });
 
-    it(`should create all nodes in tree structure 
+  it(`should create all nodes in tree structure 
         and ignore nodes that already created`, () => {
-        expect(conductors.length).toEqual(10);
-    })
+    expect(conductors.length).toEqual(10);
+  });
 
-    it(`should add children nodes in "children" property of nodes`, () => {
-        console.log(v1.children)
-        expect(v1.children.length).toEqual(1);
-        expect(v1Courses.children.length).toEqual(1);
-        expect(v1CoursesSingle.children.length).toEqual(0);
-        
-        expect(v2.children.length).toEqual(2);
-        expect(v2Courses.children.length).toEqual(0);
-        expect(v2Tests.children.length).toEqual(0); 
-        
-        expect(v3.children.length).toEqual(1);
-        expect(v3Test1.children.length).toEqual(1);
-        expect(v3Courses.children.length).toEqual(1);
-        expect(v3Test3.children.length).toEqual(0);
-    })
-})
+  it(`should add children properly 
+        when a completely new branch create`, () => {
+    controllerDecorator("v4/video/courses/single");
+    conductors = container
+      .getAll<Conductor>(ControllerType.Conductor)
+      .sort((a, b) => a.depth - b.depth);
+    const v4 = conductors.find((element) => element.name === "v4")!;
+    const v4Video = v4?.children.find((element) => element.name === "video")!;
+    const v4Courses = v4Video?.children.find((element) => element.name === "courses")!;
+    const v4Single = v4Courses?.children.find((element) => element.name === "single")!;
+
+    expect(v4.children.length).toEqual(1);
+    expect(v4Video.children.length).toEqual(1);
+    expect(v4Courses.children.length).toEqual(1);
+    expect(v4Single.children.length).toEqual(0);
+  });
+});
+
+// it(`should add children nodes in "children" property of nodes`, () => {
+//     console.log(v1.children)
+//     expect(v1.children.length).toEqual(1);
+//     expect(v1Courses.children.length).toEqual(1);
+//     expect(v1CoursesSingle.children.length).toEqual(0);
+
+//     expect(v2.children.length).toEqual(2);
+//     expect(v2Courses.children.length).toEqual(0);
+//     expect(v2Tests.children.length).toEqual(0);
+
+//     expect(v3.children.length).toEqual(1);
+//     expect(v3Test1.children.length).toEqual(1);
+//     expect(v3Courses.children.length).toEqual(1);
+//     expect(v3Test3.children.length).toEqual(0);
+// })
+// })
--- src/controller.ts
+++ src/controller.ts
@@ -3,85 +3,89 @@ import { container } from "./injector/injector.js";
 import { ControllerType } from "./injector/injector-types.js";
 import { ControllerClass } from "./controller.class.js";
 
-type Node = { name: string; depth: number; }
+type Node = { name: string; depth: number };
 
 export function Controller(endpoint: string) {
-    let nodes = createArrayOfNodesWithoutExistedOnes(endpoint);
-    try {
-        createNode(nodes);
-        addChildNodes(endpoint);
-    } catch (error) {
-        return error;
-    }
+  let nodes = createArrayOfNodesWithoutExistedOnes(endpoint);
+  createNodeTree(nodes);
+  // addChildNodes(endpoint);
 }
 
 function createArrayOfNodesWithoutExistedOnes(endpoint: string): Node[] {
-    let nodes = convertEndpointToArray(endpoint);
-    const existedNodes = (container.isBound(ControllerType.Conductor))
-        ? createStringArrayOfExistedNodes()
-        : []
-    if (existedNodes.length !== 0) nodes = removeExistedNodesFromNodesArray(existedNodes, nodes);
-    return nodes;
+  let nodes = convertEndpointToArray(endpoint);
+  const existedNodes = container.isBound(ControllerType.Conductor)
+    ? createStringArrayOfExistedNodes()
+    : [];
+  if (existedNodes.length !== 0)
+    nodes = removeExistedNodesFromNodesThatShouldCreate(existedNodes, nodes);
+  return nodes;
 }
 
-function removeExistedNodesFromNodesArray(existedNodes: Node[], stringNodes: Node[]): Node[] {
-    for (let i = 0; i < existedNodes.length; i++) {
-        const levelNodes = existedNodes.filter(node => node.depth === i).map(node => node.name);
-        if (!stringNodes[0] || !levelNodes.includes(stringNodes[0].name)) break;
-        stringNodes.shift();
-    }
-    return stringNodes;
+function removeExistedNodesFromNodesThatShouldCreate(
+  existedNodes: Node[],
+  stringNodes: Node[]
+): Node[] {
+  for (let i = 0; i < existedNodes.length; i++) {
+    const levelNodes = existedNodes
+      .filter((node) => node.depth === i)
+      .map((node) => node.name);
+    if (!stringNodes[0] || !levelNodes.includes(stringNodes[0].name)) break;
+    stringNodes.shift();
+  }
+  return stringNodes;
 }
 
 function convertEndpointToArray(endpoint: string): Node[] {
-    const nodesArray = endpoint.split("/");
-    if (nodesArray[0] === "") nodesArray.shift();
-    const modifiedNodesArray = nodesArray.map((element, index) => { return { name: element, depth: index } })
-    return modifiedNodesArray;
+  const nodesArray = endpoint.split("/");
+  if (nodesArray[0] === "") nodesArray.shift();
+  const modifiedNodesArray = nodesArray.map((element, index) => {
+    return { name: element, depth: index };
+  });
+  return modifiedNodesArray;
 }
 
-function createNode(nodes: Node[]): void | Error {
-    try {
-        for (let node of nodes) {
-            container.bind<Conductor>(ControllerType.Conductor)
-                .toDynamicValue(function () {
-                    return new ControllerClass(node.name, node.depth);
-                }).inSingletonScope();
-        }
-    } catch (error) {
-        throw new Error("can't create endpoint nodes")
+function createNodeTree(nodes: Node[]): void | Error {
+  try {
+    let childConductor: Conductor;
+    for (let i = nodes.length - 1; i >= 0 ; i--) {
+      const conductor =  new ControllerClass(nodes[i].name, nodes[i].depth);
+      container
+      .bind<Conductor>(ControllerType.Conductor)
+      .toDynamicValue(function () {
+        if(i !== nodes.length - 1) conductor.addChildren(childConductor)
+        childConductor = conductor;
+        return conductor;
+      })
+      .inSingletonScope();
     }
+  } catch (error) {
+    throw new Error("can't create endpoint nodes");
+  }
 }
 
 function addChildNodes(endPoint: string): void {
-    const nodesArray = convertEndpointToArray(endPoint);
-    const nodes = container.getAll<Conductor>(ControllerType.Conductor).sort((a, b) => a.depth - b.depth);
-    const firstLevelNodes = nodes.filter(node => node.depth === 0);
-    const node = firstLevelNodes.find(firstLevelNode => firstLevelNode.name === nodesArray[0].name);
-    const child = nodes.filter(node => node.depth === 1).find(node => node.name === nodesArray[1].name);
-    let numerator = 1;
-    while(numerator <= nodesArray.length){
-
-    }
-    for (let i = 0; i < nodesArray.length - 1; i++) {
-        
-
-
-
-
-        const levelNodesNames = nodes.filter(node => node.depth === i).map(node => node.name);
-        if (!levelNodesNames.includes(nodes[i].name)) throw new Error("can't create endpoint tree");
-        if (!nodes[i].children.includes(nodes[i + 1])) nodes[i].addChildren(nodes[i + 1])
-    }
-    // for (let i = 0; i < nodes.length - 1; i++) {
-    //     if (!nodes[i].children.includes(nodes[i + 1])) nodes[i].addChildren(nodes[i + 1]);
-    // }
+  // const nodesArray = convertEndpointToArray(endPoint);
+  // const nodes = container.getAll<Conductor>(ControllerType.Conductor).sort((a, b) => a.depth - b.depth);
+  // const firstLevelNodes = nodes.filter(node => node.depth === 0);
+  // const node = firstLevelNodes.find(firstLevelNode => firstLevelNode.name === nodesArray[0].name);
+  // const child = nodes.filter(node => node.depth === 1).find(node => node.name === nodesArray[1].name);
+  // let numerator = 1;
+  // while(numerator <= nodesArray.length){
+  // }
+  // for (let i = 0; i < nodesArray.length - 1; i++) {
+  //     const levelNodesNames = nodes.filter(node => node.depth === i).map(node => node.name);
+  //     if (!levelNodesNames.includes(nodes[i].name)) throw new Error("can't create endpoint tree");
+  //     if (!nodes[i].children.includes(nodes[i + 1])) nodes[i].addChildren(nodes[i + 1])
+  // }
+  // for (let i = 0; i < nodes.length - 1; i++) {
+  //     if (!nodes[i].children.includes(nodes[i + 1])) nodes[i].addChildren(nodes[i + 1]);
+  // }
 }
 
 function createStringArrayOfExistedNodes(): Node[] {
-    const conductors = container.getAll<Conductor>(ControllerType.Conductor);
-    const sortedConductors = conductors.sort((a, b) => a.depth - b.depth);
-    return sortedConductors.map((element: Conductor) => { return { name: element.name, depth: element.depth } });
+  const conductors = container.getAll<Conductor>(ControllerType.Conductor);
+  const sortedConductors = conductors.sort((a, b) => a.depth - b.depth);
+  return sortedConductors.map((element: Conductor) => {
+    return { name: element.name, depth: element.depth };
+  });
 }
-
-
