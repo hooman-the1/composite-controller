0cb72f341685b2d569fabb2b2ace9685bbab289c
Amirali Momtaheni <amiralimomtaheni@gmail.com>
2023-04-03 18:36:22 +0330

    14/1-2

--- src/controller.class.spec.ts
+++ src/controller.class.spec.ts
@@ -1,5 +1,25 @@
+import { Controller } from "./controller.class.js";
+import { Conductor } from "./controller.interface";
+import { ControllerType } from "./injector/injector-types.js";
+import { container } from "./injector/injector.js";
+
 describe("Controller class", () => {
-    it("", () => {
-        
+    describe("addChildren function",() => {
+        let conductorObject: Conductor;
+        beforeEach(() => {
+            const conductor = new Controller("test", 11);
+            container.bind<Conductor>(ControllerType.Conductor).toDynamicValue( () => {
+              return conductor;  
+            }).inSingletonScope()
+            conductorObject = container.get<Controller>(ControllerType.Conductor);
+        })
+
+        afterEach(() => {
+            container.unbindAll();
+        })
+        it(`should push its argument to
+            children property`, () => {
+            
+        })
     })
 })
